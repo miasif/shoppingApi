ade9d5e1a1cecf5f7e10048ff0375fa8debbed59
miasif <minhazul.islam.asif@gmail.com>
2022-01-27 16:41:19 +0600

    api done

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 app.use(express.json());
@@ -16,6 +19,9 @@ mongoose
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Backend server is running");
